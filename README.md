efd11e5b622f96f5c2681f1527fdece47f912b18
Michael Škvrňák <397925@mail.muni.cz>
2021-04-05 15:54:46 +0200

    Scraper (#4)
    * Finish scraper
    * Set up GitHub actions
    * Add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# README
+
+Scripts for scraping transfers on ANO's transparent accounts. 
+If there is no .csv file, the script scrapes as many transfers
+from the account as possible (the number is limited by the website which allows
+for 200 clicks on the 'Load more' button). 
+If there is a .csv file with scraped data, the script appends new transfers to the file.  
+
+## Usage
+### Run scrapers
+
+`node src/scraper-ano.js [account_number]`  
+
+e.g.
+
+`node src/scraper-ano.js 4070217`
--- src/scraper-ano.js
+++ src/scraper-ano.js
@@ -1,45 +1,79 @@
 const puppeteer = require('puppeteer');
 var fs = require("fs");
 
-function parseRow(row) {
-    var date = row.childNodes[0].innerText
-    var money = row.childNodes[1].innerText
-    var symbols = row.childNodes[1].innerText
-    var donationBody = row.childNodes[3].innerText.split("\n")
-    return {
-        "datum": date, 
-        "transferAmount": money, 
-        "transferSymbols": symbols, 
-        "person": donationBody[0],
-        "transferType": donationBody[1], 
-        "transferMessage": donationBody[2]
+// 4070217 - general transparent account
+// 4090453 - PSP elections
+// 4090445 - 2020 Senate elections
+
+var args = process.argv.slice(2);
+const path = 'output/ano-transfers-' + args[0] + '.csv';
+
+function findLastLine(path){
+    var lastLine;
+    try {
+        const fileContent = fs.readFileSync(path, 'UTF-8');
+        const lines = fileContent.split(/\r?\n/);
+        lastLine = lines[lines.length - 1];
+    } catch (err) {
+        lastLine = ''
     }
+    
+    return lastLine
 }
 
+var finalLine;
+finalLine = findLastLine(path);
+
+console.log(finalLine);
+
 (async () => {
+  function delay(time) {
+   return new Promise(function(resolve) { 
+       setTimeout(resolve, time)
+   });
+  }
+  
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  await page.goto('https://www.kb.cz/cs/transparentni-ucty/4070217', 
+  await page.goto('https://www.kb.cz/cs/transparentni-ucty/' + args[0], 
                     {waitUntil: 'networkidle2'});
 
-  var buttonNext = await page.evaluate(() => {
-      document.getElementsByClassName("btn-outline-secondary")[0]
-  })
-
-  var donations = await page.evaluate(() => {
-    rows = document.getElementsByTagName("tr")
-    rowsArray = Array.from(rows)
-    return rowsArray.map(x => parseRow(x))
-  });
-
-/*
-  fs.appendFile('output/champions.csv', huntersTable, function(err) {
+  var shouldStop = false;
+  var buttonStatus = false;
+  var lastLineIndex;
+  var tableBody;
+  var tableRows;
+  var i = 0;
+  
+  while (!shouldStop) {
+    await page.click('.btn-outline-secondary');
+    await delay(4000);
+    buttonStatus = await page.$eval('.btn-outline-secondary', x => x.disabled);
+    tableBody = await page.$('tbody');
+    tableRows = await page.evaluate(body => Array.from(body.getElementsByTagName("tr")).map(x => x.innerText.replaceAll(';', ':').replaceAll('\t', ';').replaceAll('\n', ';')), tableBody);
+    lastLineIndex = tableRows.indexOf(finalLine);
+    shouldStop = lastLineIndex != -1 || buttonStatus
+    i = i + 1;
+    console.log(i);
+  };
+  
+  console.log(lastLineIndex);
+  const tbody = await page.$('tbody');
+  var rows = await page.evaluate(body => Array.from(body.getElementsByTagName("tr")).map(x => x.innerText.replaceAll(';', ':').replaceAll('\t', ';').replaceAll('\n', ';')), tbody);
+  
+  if(lastLineIndex !== -1){
+    rows = '\n' + rows.slice(0, lastLineIndex).reverse().join('\n')
+  }else{
+    rows = rows.reverse().join('\n')
+  }
+  
+  await fs.appendFile(path, rows, function(err) {
     if (err) {
        return console.error(err);
     }
     console.log("Data written successfully!");
  });
- 
- */
+  
   await browser.close();
 })();
+
