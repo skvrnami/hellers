442f5dfdc2c8cf5c8a5034d1d1354f742f5e2e39
Michael Škvrňák <397925@mail.muni.cz>
2021-04-20 13:06:22 +0200

    Update analysis

--- 00_analysis.R
+++ 00_analysis.R
@@ -10,7 +10,17 @@ heller_transactions <- ano_general_account %>%
     filter(amount > 0 & amount < 10) %>% 
     count(date)
 
-ggplot(heller_transactions %>%
+data.frame(
+    date = seq.Date(from = min(heller_transactions$date), 
+                    to = max(heller_transactions$date), 
+                    by = 1)
+) -> all_dates
+
+heller_transactions_all_dates <- left_join(
+    all_dates, heller_transactions, by = "date") %>%
+    mutate(n = ifelse(is.na(n), 0, n))
+
+ggplot(heller_transactions_all_dates %>%
            filter(date >= "2021-01-01"), aes(x = date, y = n)) + 
     geom_line() + 
     geom_point() + 
