e92b48d9998d38a2a884a1eb6ed1bcdc68a662ef
Michael Škvrňák <397925@mail.muni.cz>
2021-06-03 15:03:39 +0200

    Rerun analysis and cleanup

--- test-read-csv.R
+++
@@ -1,9 +0,0 @@
-ano_ps <- read.csv("output/ano-transfers-4090453.csv", 
-                   header = FALSE, 
-                   sep = ";", 
-                   quote = "")
-
-ano_general_account <- read.csv("output/ano-transfers-4070217.csv", 
-                                header = FALSE, 
-                                sep = ";", 
-                                quote = "")
